3133b6cb29989f213c56a2bc45bc126a2c8f7804
Matthijs Groen <matthijs@kabisa.nl>
2018-04-24 13:06:48 +0200

    Start webpack build process

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true
+  },
+  plugins: ["compat", "prettier"],
+  extends: ["eslint:recommended", "prettier"],
+  parser: "babel-eslint",
+  parserOptions: {
+    sourceType: "module",
+    ecmaFeatures: {
+      modules: true,
+      experimentalObjectRestSpread: true
+    }
+  },
+  rules: {
+    "prettier/prettier": "error",
+    "compat/compat": "error"
+  }
+};
---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require("path");
+const supportedBrowsers = require("browserslist").readConfig(
+  path.join(__dirname, "browserslist")
+).defaults;
+
+const babel = {
+  presets: [
+    ["stage-3"],
+    [
+      "env",
+      {
+        targets: {
+          browsers: supportedBrowsers
+        },
+        modules: false,
+        loose: true,
+        debug: true
+      }
+    ]
+  ],
+  plugins: ["transform-class-properties"]
+};
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "build")
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, "./"),
+    publicPath: "/build/",
+    host: "127.0.0.1",
+    port: 8080,
+    open: true
+  },
+  resolve: {
+    extensions: [".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: "babel-loader",
+        exclude: /node_modules/,
+        options: babel
+      }
+    ]
+  }
+};
