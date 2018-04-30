4d98c080591ea66f7bcac408476f56b08c9cf25d
Matthijs Groen <matthijs@kabisa.nl>
2018-04-30 22:20:19 +0200

    Add exhaust flame using particles

--- src/scenes/game-play.js
+++ src/scenes/game-play.js
@@ -1,5 +1,6 @@
 import Phaser from "phaser";
 import playerShipImage from "src/assets/sprites/player-ship.png";
+import particlesImage from "src/assets/sprites/particles.png";
 
 class GamePlay extends Phaser.Scene {
   constructor() {
@@ -11,13 +12,32 @@ class GamePlay extends Phaser.Scene {
       frameWidth: 96,
       frameHeight: 64
     });
+    this.load.image("particles", particlesImage);
   }
 
   create() {
+    const particles = this.add.particles("particles");
+    const zone = new Phaser.Geom.Circle(0, 0, 10);
+    const emitter = particles.createEmitter({
+      speed: 100,
+      scale: { start: 1.5, end: 0.2 },
+      x: -50,
+      gravityX: -140,
+      angle: 180,
+      acceleration: true,
+      blendMode: "ADD",
+      emitZone: {
+        type: "random",
+        source: zone
+      }
+    });
+
     this.ship = this.physics.add.sprite(400, 300, "playerShip", 0);
     this.ship.body.allowDrag = true;
     this.ship.body.drag.x = 1000;
     this.ship.body.drag.y = 1000;
+
+    emitter.startFollow(this.ship);
 
     this.left = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
     this.right = this.input.keyboard.addKey(
