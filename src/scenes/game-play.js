836fc18b153e98699f4dd9bd9f0af2217ed3d0dd
Matthijs Groen <matthijs@kabisa.nl>
2018-05-03 13:41:24 +0200

    Add lives and score counters

--- src/scenes/game-play.js
+++ src/scenes/game-play.js
@@ -1,10 +1,17 @@
 import Phaser from "phaser";
 import playerShipImage from "src/assets/sprites/player-ship.png";
 import particlesImage from "src/assets/sprites/particles.png";
+import meteorImage from "src/assets/sprites/meteor.png";
 
 class GamePlay extends Phaser.Scene {
   constructor() {
     super({ key: "gameplay" });
+
+    this.lives = 5;
+    this.score = 0;
+    this.timeScore = 0;
+
+    this.collisionCooldowns = {};
   }
 
   preload() {
@@ -13,16 +20,43 @@ class GamePlay extends Phaser.Scene {
       frameHeight: 64
     });
     this.load.image("particles", particlesImage);
+    this.load.image("meteor", meteorImage);
   }
 
   create() {
+    this.meteors = this.physics.add.group({});
+    this.scoreText = this.add.text(10, 10, `Score: ${this.score}`);
+    this.livesText = this.add.text(400, 10, `Lives: ${this.lives}`);
+
     this.createShip();
     this.createControls();
+    this.meteorCooldown = 10e3;
+    this.spawnMeteor();
+
+    this.physics.add.overlap(
+      this.ship,
+      this.meteors,
+      this.meteorImpact,
+      this.meteorCollide,
+      this
+    );
+  }
+
+  meteorCollide(ship, meteor) {
+    if (this.collisionCooldowns[meteor]) {
+      return false;
+    }
+    this.collisionCooldowns[meteor] = 500;
+    return true;
+  }
+
+  meteorImpact() {
+    this.lives -= 1;
   }
 
   createShip() {
     const particles = this.add.particles("particles");
-    const zone = new Phaser.Geom.Circle(0, 0, 10);
+    const zone = new Phaser.Geom.Circle(0, 0, 5);
     const emitter = particles.createEmitter({
       speed: 100,
       scale: { start: 1.5, end: 0.2 },
@@ -49,8 +83,33 @@ class GamePlay extends Phaser.Scene {
     this.cursors = this.input.keyboard.createCursorKeys();
   }
 
-  update() {
+  spawnMeteor() {
+    const meteor = this.add.image(600, 200, "meteor");
+    meteor.depth = -1;
+    this.meteors.add(meteor);
+  }
+
+  update(time, delta) {
+    this.meteorCooldown -= delta;
+    if (this.meteorCooldown < 0) {
+      this.meteorCooldown = 5e3 + Math.random() * 1000;
+    }
     this.updateShipControls();
+    this.timeScore = Math.floor(time / 3000) * 10;
+
+    for (let obj in this.collisionCooldowns) {
+      this.collisionCooldowns[obj] -= delta;
+      if (this.collisionCooldowns[obj] <= 0) {
+        delete this.collisionCooldowns[obj];
+      }
+    }
+
+    if (this.score < this.timeScore) {
+      this.score += 1;
+    }
+
+    this.scoreText.setText(`Score: ${this.score}`);
+    this.livesText.setText(`Lives: ${this.lives}`);
   }
 
   updateShipControls() {
