584d4bff1bc14e2d46a17ed9411cecf5ccaa8a48
Matthijs Groen <matthijs@kabisa.nl>
2018-05-14 13:32:22 +0200

    Refactor ship code into ship class

--- src/scenes/game-play.js
+++ src/scenes/game-play.js
@@ -3,6 +3,46 @@ import playerShipImage from "src/assets/sprites/player-ship.png";
 import particlesImage from "src/assets/sprites/particles.png";
 import meteorImage from "src/assets/sprites/meteor.png";
 
+class PlayerShip extends Phaser.Physics.Arcade.Sprite {
+  constructor(scene) {
+    super(scene, 400, 300, "playerShip", 0);
+
+    scene.sys.displayList.add(this);
+    scene.sys.updateList.add(this);
+    scene.physics.world.enableBody(this, Phaser.Physics.Arcade.DYNAMIC_BODY);
+
+    this.setupTailFlame();
+    this.setupPhysicalProperties();
+  }
+
+  setupTailFlame() {
+    const particles = this.scene.add.particles("particles");
+    const zone = new Phaser.Geom.Circle(0, 0, 5);
+    const emitter = particles.createEmitter({
+      speed: 100,
+      scale: { start: 1.5, end: 0.2 },
+      x: -50,
+      gravityX: -140,
+      angle: 180,
+      acceleration: true,
+      blendMode: "ADD",
+      emitZone: {
+        type: "random",
+        source: zone
+      }
+    });
+
+    emitter.startFollow(this);
+  }
+
+  setupPhysicalProperties() {
+    this.body.allowDrag = true;
+    this.body.drag.x = 500;
+    this.body.drag.y = 500;
+    this.setCollideWorldBounds(true);
+  }
+}
+
 class GamePlay extends Phaser.Scene {
   constructor() {
     super({ key: "gameplay" });
@@ -58,28 +98,7 @@ class GamePlay extends Phaser.Scene {
   }
 
   createShip() {
-    const particles = this.add.particles("particles");
-    const zone = new Phaser.Geom.Circle(0, 0, 5);
-    const emitter = particles.createEmitter({
-      speed: 100,
-      scale: { start: 1.5, end: 0.2 },
-      x: -50,
-      gravityX: -140,
-      angle: 180,
-      acceleration: true,
-      blendMode: "ADD",
-      emitZone: {
-        type: "random",
-        source: zone
-      }
-    });
-
-    this.ship = this.physics.add.sprite(400, 300, "playerShip", 0);
-    this.ship.body.allowDrag = true;
-    this.ship.body.drag.x = 500;
-    this.ship.body.drag.y = 500;
-    emitter.startFollow(this.ship);
-    this.ship.setCollideWorldBounds(true);
+    this.ship = new PlayerShip(this);
   }
 
   createControls() {
